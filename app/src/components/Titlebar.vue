9264bdee04e5577a432362dfab00ea6eeb13627f
Cameron Charlton <charltonc98@gmail.com>
2020-10-08 12:09:59 +0100

    Update Titlebar.vue
    Dragging the titlebar now drags the window.
    
    Titlebar icons now don't stay highlighted after clicking.

--- app/src/components/Titlebar.vue
+++ app/src/components/Titlebar.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-app-bar
+      class="titlebar"
       dark
       fixed
       dense
@@ -16,9 +17,9 @@
           align-left
         />
         <v-spacer></v-spacer>
-        <v-icon id="minimise" v-on:click="minimizeWindow" size="20" class="mr-5">mdi-minus</v-icon>
-        <v-icon id="maximise" v-on:click="maximizeWindow" size="20" class="mr-5">mdi-checkbox-blank-outline</v-icon>
-        <v-icon id="close" v-on:click="closeWindow" size="20">mdi-close</v-icon>
+        <v-icon id="minimise" v-on:click="minimizeWindow" size="20" class="minimise">mdi-minus</v-icon>
+        <v-icon id="maximise" v-on:click="maximizeWindow" size="20" class="maximise">mdi-checkbox-blank-outline</v-icon>
+        <v-icon id="close" v-on:click="closeWindow" size="20" class="close">mdi-close</v-icon>
     </v-app-bar>
   </div>
 </template>
@@ -38,17 +39,37 @@ export default {
     {
         var window = remote.BrowserWindow.getFocusedWindow();
         window.close();
+        document.activeElement.blur()
     },
 
     minimizeWindow () {  
         var window = remote.BrowserWindow.getFocusedWindow();
         window.minimize();
+        document.activeElement.blur()
     },
 
     maximizeWindow () {
         var window = remote.BrowserWindow.getFocusedWindow();
         window.isMaximized() ? window.unmaximize() : window.maximize();
+        document.activeElement.blur()
     }
   }
 };
 </script>
+<style>
+  .titlebar {
+  -webkit-user-select: none;
+  -webkit-app-region: drag;
+  }
+  .minimise {
+    -webkit-app-region: no-drag;
+    margin-right: 20px;
+  }
+  .maximise {
+    -webkit-app-region: no-drag;
+    margin-right: 20px;
+  }
+  .close {
+    -webkit-app-region: no-drag;
+  }
+</style>
